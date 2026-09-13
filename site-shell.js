611fd805457c848a341c674bab586313a996a785
REDACTED <87097418+KNOBSOCK@users.noreply.github.com>
2026-09-13 02:39:27 -0400

    Limit shell fade to music transitions

--- site-shell.js
+++ site-shell.js
@@ -84,7 +84,7 @@
     const pathname = routeAliases[url.pathname] || url.pathname;
     const params = new URLSearchParams(url.search);
     params.set('site-content', '1');
-params.set('shell-version', '20260913-5');
+params.set('shell-version', '20260913-6');
     const query = params.toString();
     return `${pathname}${query ? `?${query}` : ''}${url.hash}`;
   }
@@ -601,7 +601,7 @@ params.set('shell-version', '20260913-5');
 
   attachFrame(routeFrame);
   attachFrame(musicFrame);
-musicFrame.src = '/music.html?site-content=1&shell-version=20260913-5';
+musicFrame.src = '/music.html?site-content=1&shell-version=20260913-6';
 
   const initialParams = new URLSearchParams(window.location.search);
   const initialRoute = initialParams.get('route') || '/index.html';
